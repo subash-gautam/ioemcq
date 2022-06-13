3a4d090668de8b3ac6cbe7aa1512a1f410e17968
Subash Gautam <83286019+subash-gautam@users.noreply.github.com>
2022-06-13 15:15:03 +0545

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,27 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Prashna k ho? ",
+    answer: "prashna k ho bhanda kheri .....",
+    options: [
+      "prashna k ho bhanda kheri .....",
+      "malaai achamma lagxa.",
+      "tapaai chup lagnus",
+      "china sanga 12 arba magau, india sanga ....",
+    ],
+    solution: "https://www.youtube.com/c/ioestudy"
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ],
+    solution: "https://www.youtube.com/c/ioestudy"
+  },
+];
